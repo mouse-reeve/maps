a81b07225171fa3dea51557f1093a83583134aea
Mouse Reeve <mousereeve@riseup.net>
2018-01-18 20:39:40 -0500

    Tweaking topo borders and colors

--- map.js
+++ map.js
@@ -1,9 +1,12 @@
+var black;
+
 function setup() {
     var container = document.getElementById('map');
     var canvas = createCanvas(640, 360);
     canvas.parent(container);
 
     var seed = container.getAttribute('data-seed');
+    black = color(0);
 
     var map = new Map(seed);
     map.draw_map();
@@ -27,7 +30,7 @@ class Map {
 
         this.colors = {
             water: '#A9DCE0',
-            topo: ['#C1CCA5', '#E6F0BF', '#E9EFB5', '#DAC689', '#CDA37F', '#CB9082', '#C8BEC6', '#D6D5E5'],
+            topo: ['#C1CCA5', '#C1CCA5', '#E6F0BF', '#E9EFB5', '#DAC689', '#CDA37F', '#CB9082', '#C8BEC6', '#D6D5E5'],
         };
     }
 
@@ -35,23 +38,26 @@ class Map {
         // ----- compute elements ----- \\
         this.get_topography();
 
-        var comparison = function (x, y, i, j, elevation) {
-        }
+        var color_gap = 5;
         // ----- draw map ------------- \\
         for (var y = 0; y < height; y++) {
             for (var x = 0; x < width; x++) {
                 // bucketize for topo map
                 var value = Math.floor(this.elevation[x][y] * 100);
-                var color = this.colors.topo[Math.floor(value / 10)];
+                var color_bucket = Math.floor(value / color_gap);
+                if (color_bucket >= this.colors.topo.length) {
+                    color_bucket = this.colors.topo.length - 1;
+                }
+                var point_color = this.colors.topo[color_bucket];
 
                 var border_value = this.topo_border(x, y);
                 if (!!border_value) {
-                    var bucket1 = Math.floor(border_value[0] * 100 / 10);
-                    var bucket2 = Math.floor(border_value[1] * 100 / 10);
+                    var bucket1 = Math.floor(border_value[0] * 100 / color_gap);
+                    var bucket2 = Math.floor(border_value[1] * 100 / color_gap);
 
-                    color = bucket1 != bucket2 ? 0 : 150;
+                    point_color = bucket1 != bucket2 ? 0 : lerpColor(color(point_color), black, 0.3);
                 }
-                stroke(color);
+                stroke(point_color);
                 point(x, y);
             }
         }
@@ -76,19 +82,23 @@ class Map {
     get_topography() {
         for (var y = 0; y < height; y++) {
             for (var x = 0; x < width; x++) {
-                var frequency = 2 / width;
+                // higher number -> "zoom out"
+                var frequency = 3 / width;
+
                 var nx = x * frequency - 0.5;
                 var ny = y * frequency - 0.5;
 
+                // noisiness of edges
                 var octaves = 3;
+
                 var noise_value = 0;
                 var divisor = 1;
                 for (var i = 1; i <= octaves; i = i * 2) {
                     noise_value += 1 / i * this.get_noise(i * nx, i * ny);
                     divisor += 1 / i;
                 }
-                noise_value = noise_value / divisor;
-                noise_value = Math.pow(noise_value, 2);
+                noise_value = noise_value / divisor; // keeps the value between 0 and 1
+                noise_value = Math.pow(noise_value, 2); // flatens out the lows
 
                 this.elevation[x][y] = noise_value;
             }
