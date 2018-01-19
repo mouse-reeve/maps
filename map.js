1a9519ce64576f61456ee292819a50c30b169f48
Mouse Reeve <mousereeve@riseup.net>
2018-01-18 20:23:03 -0500

    Topo map with border
    Hell yeah

--- map.js
+++ map.js
@@ -1,5 +1,4 @@
 function setup() {
-
     var container = document.getElementById('map');
     var canvas = createCanvas(640, 360);
     canvas.parent(container);
@@ -25,22 +24,53 @@ class Map {
         }
 
         this.elevation = this.create_matrix();
+
+        this.colors = {
+            water: '#A9DCE0',
+            topo: ['#C1CCA5', '#E6F0BF', '#E9EFB5', '#DAC689', '#CDA37F', '#CB9082', '#C8BEC6', '#D6D5E5'],
+        };
     }
 
     draw_map() {
         // ----- compute elements ----- \\
         this.get_topography();
 
+        var comparison = function (x, y, i, j, elevation) {
+        }
         // ----- draw map ------------- \\
         for (var y = 0; y < height; y++) {
             for (var x = 0; x < width; x++) {
-                var value = this.elevation[x][y] * 255;
                 // bucketize for topo map
-                value = value - value % 10;
-                stroke(value);
+                var value = Math.floor(this.elevation[x][y] * 100);
+                var color = this.colors.topo[Math.floor(value / 10)];
+
+                var border_value = this.topo_border(x, y);
+                if (!!border_value) {
+                    var bucket1 = Math.floor(border_value[0] * 100 / 10);
+                    var bucket2 = Math.floor(border_value[1] * 100 / 10);
+
+                    color = bucket1 != bucket2 ? 0 : 150;
+                }
+                stroke(color);
                 point(x, y);
             }
         }
+    }
+
+    topo_border(x, y) {
+        var granularity = 50;
+        for (var i = 0; i <= 1; i++) {
+            for (var j = 0; j <= 1; j++) {
+                if (x + i >= 0 && x + i < width && y + j >= 0 && y + j < height) {
+                    var elev1 = Math.floor(this.elevation[x][y] * granularity);
+                    var elev2 = Math.floor(this.elevation[x + i][y + j] * granularity);
+                    if (elev1 != elev2) {
+                        return [this.elevation[x][y], this.elevation[x + i][y + j]];
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     get_topography() {
@@ -58,7 +88,7 @@ class Map {
                     divisor += 1 / i;
                 }
                 noise_value = noise_value / divisor;
-                noise_value = Math.pow(noise_value, 1.5);
+                noise_value = Math.pow(noise_value, 2);
 
                 this.elevation[x][y] = noise_value;
             }
