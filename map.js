c459ed8481ed7a559234de37e2611e7c84a1193e
Mouse Reeve <mousereeve@riseup.net>
2018-01-29 17:51:21 -0500

    Always smooth riverbanks

--- map.js
+++ map.js
@@ -243,9 +243,7 @@ class Map {
                     var h_distance = Math.sqrt(Math.pow(this.river[j][0] - x, 2) + Math.pow(this.river[j][1] - y, 2));
                     distance = h_distance < distance ? h_distance : distance;
                 }
-                if (distance < 100) {
-                    this.elevation[x][y] -= 4 / (distance ** 1.5);
-                }
+                this.elevation[x][y] -= 4 / (distance ** 1.5);
             }
         }
     }
